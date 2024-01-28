e8ebb23e80024df68be42c16f0b81d6b20e26dfc
ERN VENTURES <102434694+ERNcrypto@users.noreply.github.com>
2024-01-28 15:39:34 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ cd || return
 rm -rf paloma
 git clone https://github.com/palomachain/paloma.git
 cd paloma || return
-git checkout v1.10.2
+git checkout v1.11.0
 make install
 sudo mv -f $HOME/go/bin/palomad /usr/local/bin/palomad
 palomad version 
